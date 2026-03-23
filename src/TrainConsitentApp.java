dff15de1595e4247e1cd66a9617ae41f3ff8e6b5
AasthaSingh15 <aasthasingh9532@gmail.com>
2026-03-23 09:32:45 +0530

    Advanced Train Operations

--- src/TrainConsitentApp.java
+++ src/TrainConsitentApp.java
@@ -2,14 +2,19 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 import java.util.HashSet;
+import java.util.LinkedList;
 
 public class TrainConsitentApp {
     public static void main(String[] args) {
+
         System.out.println("===================================");
         System.out.println(" === Train Consist Management App === ");
         System.out.println("===================================");
 
-        System.out.println("Train initialized successfully....");
+        // =========================
+        // UC1 - Initialization
+        // =========================
+        System.out.println("\nTrain initialized successfully...");
 
         List<String> trainConsist = new ArrayList<>();
 
@@ -18,11 +23,13 @@ public class TrainConsitentApp {
 
         System.out.println("System ready for operations...");
 
-        System.out.println("=================================");
-        System.out.println("UC2 - Add Passenger Bogies to Train");
+        // =========================
+        // UC2 - Add / Remove Bogies
+        // =========================
+        System.out.println("\n=================================");
+        System.out.println("UC2 - Add Passenger Bogies");
         System.out.println("=================================");
 
-        // Add bogies
         trainConsist.add("Sleeper");
         trainConsist.add("AC Chair");
         trainConsist.add("First Class");
@@ -30,34 +37,65 @@ public class TrainConsitentApp {
         System.out.println("After Adding Bogies:");
         System.out.println("Passenger Bogies: " + trainConsist);
 
-        // Remove bogie
-        System.out.println("After Removing 'AC Chair':");
         trainConsist.remove("AC Chair");
+
+        System.out.println("After Removing 'AC Chair':");
         System.out.println("Passenger Bogies: " + trainConsist);
 
-        // Check existence
-        System.out.println("Checking if 'Sleeper' exists:");
         System.out.println("Contains Sleeper? " + trainConsist.contains("Sleeper"));
 
         System.out.println("Final Train Passenger Consist:");
         System.out.println(trainConsist);
 
-        System.out.println("=================================");
-        System.out.println("UC3 - Track Unique Bogie IDs");
+        // =========================
+        // UC3 - Unique Bogie IDs
+        // =========================
+        System.out.println("\n=================================");
+        System.out.println("UC3 - Unique Bogie ID Management");
         System.out.println("=================================");
 
-        // Create a HashSet for bogie IDs
         Set<String> bogieIds = new HashSet<>();
 
-        // Add bogie IDs (including duplicates)
         bogieIds.add("BG101");
         bogieIds.add("BG102");
         bogieIds.add("BG103");
-        bogieIds.add("BG101"); // Duplicate
-        bogieIds.add("BG102"); // Duplicate
+        bogieIds.add("BG101"); // duplicate
+        bogieIds.add("BG102"); // duplicate
 
-        // Display unique bogie IDs
-        System.out.println("Bogies IDs After Insertion:");
+        System.out.println("Unique Bogie IDs:");
         System.out.println(bogieIds);
+
+        // =========================
+        // UC4 - LinkedList Operations
+        // =========================
+        System.out.println("\n=================================");
+        System.out.println("UC4 - Advanced Train Operations");
+        System.out.println("=================================");
+
+        LinkedList<String> linkedTrain = new LinkedList<>();
+
+        linkedTrain.add("Engine");
+        linkedTrain.add("Sleeper");
+        linkedTrain.add("AC");
+        linkedTrain.add("Cargo");
+        linkedTrain.add("Guard");
+
+        System.out.println("Initial Train Consist:");
+        System.out.println(linkedTrain);
+
+        // Insert Pantry at index 2
+        linkedTrain.add(2, "Pantry Car");
+
+        System.out.println("\nAfter Inserting Pantry  Car at position 2:");
+        System.out.println(linkedTrain);
+
+        // Remove first and last
+        linkedTrain.removeFirst();
+        linkedTrain.removeLast();
+
+        System.out.println("\nAfter removing first and last bogie:");
+        System.out.println(linkedTrain);
+
+
     }
 }
