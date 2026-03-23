7e46c07aaee087f3fcc2dc4c7bdfd8f93308c6b1
AasthaSingh15 <aasthasingh9532@gmail.com>
2026-03-23 08:53:02 +0530

    UC1: Train consist management module

--- src/TrainConsitentApp.java
+++ src/TrainConsitentApp.java
@@ -1,2 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class TrainConsitentApp {
+    public static void main(String[] args) {
+        System.out.println("===================================");
+        System.out.println(" ===Train Consist Management App === ");
+        System.out.println(" ==================================");
+
+        System.out.println("Train initialized successfully....");
+        List<String> trainConsist = new ArrayList<>();
+        System.out.println("Initial bogie count: " + trainConsist.size());
+        System.out.println("Current train consist: " + trainConsist);
+
+        System.out.println("System ready for operations...");
+    }
 }
