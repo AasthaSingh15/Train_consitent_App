a66a58ad528ddc5662853713314cac7650b871dc
AasthaSingh15 <aasthasingh9532@gmail.com>
2026-03-23 09:11:01 +0530

    UC2: Add passenger bogies

--- src/TrainConsitentApp.java
+++ src/TrainConsitentApp.java
@@ -13,5 +13,21 @@ public class TrainConsitentApp {
         System.out.println("Current train consist: " + trainConsist);
 
         System.out.println("System ready for operations...");
+        System.out.println("=================================");
+        System.out.println("UC2 - Add Passenger Bogies to Train");
+        System.out.println("==================================");
+        System.out.println("After Adding Bogies:");
+        trainConsist.add("Sleeper");
+        trainConsist.add("AC Chair");
+        trainConsist.add("First Class");
+        System.out.println("Passenger Bogies :" + trainConsist);
+        System.out.println("After Removing 'AC Chair':");
+        trainConsist.remove("AC Chair");
+        System.out.println("Passenger Bogies :" + trainConsist);
+        System.out.println("Checking if 'Sleeper' exist:");
+        System.out.println("Contains sleeper? :" + trainConsist.contains("Sleeper"));
+        System.out.println("Final Train Passenger Consist:");
+        System.out.println(trainConsist);
+
     }
 }
