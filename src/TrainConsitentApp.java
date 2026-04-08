ab46b2551c246a44bcae115d5c4bb24a5fc7a3c4
Aastha <aasthasingh9532@gmail.com>
2026-04-08 05:59:23 +0530

    Added UC8 - Stream filtering

--- src/TrainConsitentApp.java
+++ src/TrainConsitentApp.java
@@ -6,11 +6,11 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public class TrainConsitentApp {
 
-    // ✅ UC7 Inner Class
-    static class Bogie {
+   static class Bogie {
         String name;
         int capacity;
 
@@ -182,5 +182,25 @@ public class TrainConsitentApp {
         }
 
         System.out.println("\nUC7 sorting completed...");
+        System.out.println("\n=================================");
+        System.out.println("UC8 - Filter Passenger Bogies Using Streams");
+        System.out.println("=================================");
+
+        // All bogies
+        System.out.println("\nAll Bogies:");
+        for (Bogie b : bogies) {
+            System.out.println(b.name + " -> " + b.capacity);
+        }
+
+        // Filter condition (> 60)
+        List<Bogie> filtered = bogies.stream()
+                .filter(b -> b.capacity > 60)
+                .collect(Collectors.toList());
+
+        System.out.println("\nFiltered Bogies (Capacity > 60):");
+        for (Bogie b : filtered) {
+            System.out.println(b.name + " -> " + b.capacity);
+        }
+
     }
 }
